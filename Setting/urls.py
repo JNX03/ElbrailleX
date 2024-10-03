2ce4c3300b76d6956ca7f1bf5c2d89801722f867
Jnx03 <jn03official@gmail.com>
2024-10-03 09:58:00 +0700

    Resolved Patched

--- Setting/urls.py
+++ Setting/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from Setting import views
+from .models import FileUpload
+
 
 urlpatterns = [
   path("Connect", views.CM, name='Connect'),
   path("TT", views.tt, name='Tt'),
+  path('upload/', views.upload_file, name='upload_file'),
+  path('upload/success/', views.upload_success, name='file_upload_success'),
+  path('files/', views.file_list, name='file_list'),
 ]
--- Setting/views.py
+++ Setting/views.py
@@ -5,3 +5,21 @@ def tt(request):
 
 def CM(request):
     return render(request, 'connnect.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = form.save()  # Save the file and get the model instance
+            # Redirect to success view with file URL
+            return render(request, 'upload_success.html', {'file_url': uploaded_file.file.url})
+    else:
+        form = FileUploadForm()
+    return render(request, 'upload.html', {'form': form})
+
+def file_list(request):
+    files = FileUpload.objects.all()  # Retrieve all uploaded files
+    return render(request, 'file_list.html', {'files': files})
+
+def upload_success(request):
+    return render(request, 'upload_success.html')
