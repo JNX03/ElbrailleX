5892ede9f1a261831bea223bdbfc263a66bd6274
Jnx03 <jn03official@gmail.com>
2024-10-03 09:59:09 +0700

    File Fix

--- Setting/views.py
+++ Setting/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FileUploadForm
+from .models import FileUpload
 
 def tt(request):
     return render(request, 'TT.html')
