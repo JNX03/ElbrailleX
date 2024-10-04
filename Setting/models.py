7042db6a865d4923a79c44f0997237f995e63c3e
Jnx03 <jn03official@gmail.com>
2024-10-04 09:37:02 +0700

    Patched (move to local)

--- Setting/models.py
+++ Setting/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class FileUpload(models.Model):
     title = models.CharField(max_length=100)
-    file = models.FileField(upload_to='uploads/')  # Files will be uploaded to EbrailleAssets/uploads/
+    file = models.FileField(upload_to='uploads/')  # Files will be uploaded to media/uploads/
     uploaded_at = models.DateTimeField(auto_now_add=True)
